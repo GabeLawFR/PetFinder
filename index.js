5cf61b2563313ed38f9482b040342705976bc028
GabeLawFR <gabriel.lawson@live.fr>
2023-08-31 14:15:51 -0700

    final edit

--- index.js
+++ index.js
@@ -25,21 +25,21 @@ app.get('/api/v1/pets', (req, res) => {
     res.send(pets);
 });
 
-// Same thing as the previous function but this time by specifying the pet's owner's name stored in the dog obeject array as a query by using .find to match the requested owner name with the correct one in the dog array /owner?owner=""
+// Same thing as the previous function but this time by specifying the pet's owner's name stored in the pets object array as a query by using .find to match the requested owner name with the correct one in the dog array /owner?owner=""
 app.get('/api/v1/pets/owner', (req, res) => {
     const { owner } = req.query; 
     const pet = pets.find(pet => pet.owner === owner);
     res.send(pet);
 });
 
-// Same as previous function but this time using the pet's name as a parameter to GET and display matching dog
+// Same as previous function but this time using the pet's name as a parameter to GET and display matching dog /pets/"name of pet" by using .find as well to match params name with corresponding data.js one
 app.get('/api/v1/pets/:name', (req, res) => {
     const { name } = req.params;
     const pet = pets.find(pet => pet.name === name);
     res.send(pet);
 });
 
-// Using pet's "id" as a query to GET and display matching dog
+// Using pet's "id" as a query to GET and display matching dog /pets-query?id="" using .find and parseInt to match query id with corresponding data.js one
 app.get('/api/v1/pets-query', (req, res) => {
     const { id } = req.query;
     const pet = pets.find(pet => pet.id === parseInt(id));
